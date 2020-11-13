574b610603c1a81a37daff52a8f3af7e9440bfc8
xqwen <william.xqwen@gmail.com>
2020-11-13 09:28:14 -0500

    update Makefile

--- promise_and_limitation_paper/LD_mismatching/README.md
+++ promise_and_limitation_paper/LD_mismatching/README.md
@@ -10,4 +10,7 @@ The eQTL data are simulated using the genotype data from FIN. Five GWAS data set
 The causal GWAS variants, causal eQTL variants, and the colocalized variants are pre-determined by a sampling process. Particularly, the causal GWAS variants (and the corresponding colocalization sites) remain invariant across all simulated GWAS data sets. In total, there are 2107 genes that truly harbor a colocalized.
 
 
+## Running analysis
+
+Run ```make``` to start the analysis process. Because of the stochastic nature of the enrichment estimation (by multiple imputation), there may be slight numerical difference from the reported values in the paper.
 
--- promise_and_limitation_paper/LD_mismatching/scripts/power.R
+++ promise_and_limitation_paper/LD_mismatching/scripts/power.R
@@ -1,7 +1,7 @@
-truth  = read.table("true.coloc.gene")$V1
+truth  = read.table("sim_data/true.coloc.gene")$V1
 
 
-d = read.table("fin_vs_fin.enloc.gene.out")
+d = read.table("fastenloc_out/fin_vs_fin.enloc.gene.out")
 lfdr = sort(1-d$V2)
 FDR = cumsum(lfdr)/1:length(lfdr)
 thresh = 1 - lfdr[max(which(FDR<=0.05))]
@@ -13,31 +13,7 @@ length(rej)
 print("====")
 
 
-d = read.table("fin_vs_gbr.enloc.gene.out")
-lfdr = sort(1-d$V2)
-FDR = cumsum(lfdr)/1:length(lfdr)
-thresh = 1 - lfdr[max(which(FDR<=0.05))]
-rej = d$V1[d$V2>thresh]
-int = intersect(rej,truth)
-(length(rej)-length(int))/length(rej)
-length(int)/length(truth)
-length(rej)
-print("====")
-
-
-
-d = read.table("fin_vs_tsi.enloc.gene.out")
-lfdr = sort(1-d$V2)
-FDR = cumsum(lfdr)/1:length(lfdr)
-thresh = 1 - lfdr[max(which(FDR<=0.05))]
-rej = d$V1[d$V2>thresh]
-int = intersect(rej,truth)
-(length(rej)-length(int))/length(rej)
-length(int)/length(truth)
-length(rej)
-print("====")
-
-d = read.table("fin_vs_ceu.enloc.gene.out")
+d = read.table("fastenloc_out/fin_vs_gbr.enloc.gene.out")
 lfdr = sort(1-d$V2)
 FDR = cumsum(lfdr)/1:length(lfdr)
 thresh = 1 - lfdr[max(which(FDR<=0.05))]
@@ -50,7 +26,31 @@ print("====")
 
 
 
-d = read.table("fin_vs_yri.enloc.gene.out")
+d = read.table("fastenloc_out/fin_vs_tsi.enloc.gene.out")
+lfdr = sort(1-d$V2)
+FDR = cumsum(lfdr)/1:length(lfdr)
+thresh = 1 - lfdr[max(which(FDR<=0.05))]
+rej = d$V1[d$V2>thresh]
+int = intersect(rej,truth)
+(length(rej)-length(int))/length(rej)
+length(int)/length(truth)
+length(rej)
+print("====")
+
+d = read.table("fastenloc_out/fin_vs_ceu.enloc.gene.out")
+lfdr = sort(1-d$V2)
+FDR = cumsum(lfdr)/1:length(lfdr)
+thresh = 1 - lfdr[max(which(FDR<=0.05))]
+rej = d$V1[d$V2>thresh]
+int = intersect(rej,truth)
+(length(rej)-length(int))/length(rej)
+length(int)/length(truth)
+length(rej)
+print("====")
+
+
+
+d = read.table("fastenloc_out/fin_vs_yri.enloc.gene.out")
 lfdr = sort(1-d$V2)
 FDR = cumsum(lfdr)/1:length(lfdr)
 thresh = 1 - lfdr[max(which(FDR<=0.05))]
