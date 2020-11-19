fc5ad5d6b742964c00ded958c511bb4e6a270146
xqwen <william.xqwen@gmail.com>
2020-11-19 12:05:57 -0500

    update marginal power calculation

--- promise_and_limitation_paper/empirical_power_evaluation/scripts/marginal_association.R
+++ promise_and_limitation_paper/empirical_power_evaluation/scripts/marginal_association.R
@@ -15,7 +15,7 @@ gwas_total = length(which(gwas_eff!=0))
 gwas_fdr = length(which(d_gwas$V2>=gwas_thresh & d_gwas$V3 == 0))/length(which(d_gwas$V2>=gwas_thresh))
 gwas_power =  length(which(d_gwas$V2>=gwas_thresh & d_gwas$V3 == 1))/gwas_total
 
-cat(paste0("GWAS (FDR 5%):   Realized FDR ", round(gwas_fdr,3), "   Power ", round(gwas_power,3), " (", length(which(d_gwas$V2>=gwas_thresh & d_gwas$V3 == 1)),"/",gwas_total,")"),'\n')
+#cat(paste0("GWAS (FDR 5%):   SPIP Cutoff  ", round(gwas_thresh,3),  " Realized FDR ", round(gwas_fdr,3), "   Power ", round(gwas_power,3), " (", length(which(d_gwas$V2>=gwas_thresh & d_gwas$V3 == 1)),"/",gwas_total,")"),'\n')
 
 
 
@@ -27,7 +27,9 @@ eqtl_total = length(which(eqtl_eff!=0))
 eqtl_fdr = length(which(d_eqtl$V2>=eqtl_thresh & d_eqtl$V3 == 0))/length(which(d_eqtl$V2>=eqtl_thresh))
 eqtl_power =  length(which(d_eqtl$V2>=eqtl_thresh & d_eqtl$V3 == 1))/eqtl_total
 
-cat(paste0("eQTL (FDR 5%):   Realized FDR ", round(eqtl_fdr,3), "   Power ", round(eqtl_power,3), " (", length(which(d_eqtl$V2>=eqtl_thresh & d_eqtl$V3 == 1)),"/",eqtl_total,")"), '\n')
+#cat(paste0("eQTL (FDR 5%):   SPIP Cutoff ", round(eqtl_thresh,3), " Realized FDR ", round(eqtl_fdr,3), "   Power ", round(eqtl_power,3), " (", length(which(d_eqtl$V2>=eqtl_thresh & d_eqtl$V3 == 1)),"/",eqtl_total,")"), '\n')
 
 
-
+d = read.table("results/fastenloc.est_prior.summary", head=T)
+cat(paste0("GWAS power in colocalized sites:  ", round(length(d$GWAS_spip[d$GWAS_spip >= gwas_thresh])/length(d$GWAS_spip),3)," (",length(d$GWAS_spip[d$GWAS_spip >= gwas_thresh]),"/",length(d$GWAS_spip),")\n"))
+cat(paste0("eQTL power in colocalized sites:  ", round(length(d$eQTL_spip[d$eQTL_spip >= eqtl_thresh])/length(d$eQTL_spip),3)," (",length(d$eQTL_spip[d$eQTL_spip >= eqtl_thresh]),"/",length(d$eQTL_spip),")\n"))
